ded3e1856e69b459efefd4459309414ace9a7b48
DtrTech <garyjee@karunasarawak.com>
2025-10-07 10:32:42 +0800

    aaa

--- app/Http/Controllers/HouseOwnerShipController.php
+++ app/Http/Controllers/HouseOwnerShipController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Spatie\Browsershot\Browsershot;
 use Illuminate\Http\Request;
-use App\Models\HouseOwnerShip;
+use App\Models\HouseOwnership;
 use Bouncer;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
@@ -15,7 +15,7 @@ class HouseOwnerShipController extends Controller
 {
     public function index()
     {
-        $house_ownership = HouseOwnerShip::all();
+        $house_ownership = HouseOwnership::all();
         return view('house_ownership.index')->with('house_ownership', $house_ownership);
     }
 
@@ -26,24 +26,24 @@ class HouseOwnerShipController extends Controller
 
     public function store(Request $request)
     {
-        HouseOwnerShip::create($request->all());
+        HouseOwnership::create($request->all());
         return redirect()->route('house_ownership.index')->withSuccess('House Ownership saved');
     }
 
-    public function edit(HouseOwnerShip $house_ownership)
+    public function edit(HouseOwnership $house_ownership)
     {
         return view('house_ownership.create')->with('house_ownership', $house_ownership);
     }
 
-    public function update(Request $request, HouseOwnerShip $house_ownership)
+    public function update(Request $request, HouseOwnership $house_ownership)
     {
         $house_ownership->update($request->all());
-        return redirect()->route('house_ownership.index')->withSuccess('HouseOwnerShip updated');
+        return redirect()->route('house_ownership.index')->withSuccess('HouseOwnership updated');
     }
 
-    public function destroy(HouseOwnerShip $house_ownership)
+    public function destroy(HouseOwnership $house_ownership)
     {
         $house_ownership->delete();
-        return redirect()->route('house_ownership.index')->withSuccess('HouseOwnerShip deleted');
+        return redirect()->route('house_ownership.index')->withSuccess('HouseOwnership deleted');
     }
 }
